59bd2a8f3b2fe255f71214a636dcf49fe864f8b9
cullenbohannon <65356707+cullenbohannon@users.noreply.github.com>
2020-10-31 10:34:50 +0000

    Update heroPost link

--- components/hero-post.js
+++ components/hero-post.js
@@ -14,12 +14,12 @@ export default function HeroPost({
   return (
     <section>
       <div className="mb-8 md:mb-16">
-        <CoverImage title={title} src={coverImage} slug={slug} />
+        <CoverImage title={title} src={coverImage} slug={slug} link={link} />
       </div>
       <div className="md:grid md:grid-cols-2 md:col-gap-16 lg:col-gap-8 mb-20 md:mb-28">
         <div>
           <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
-            <Link as={`/posts/${slug}`} href="/posts/[slug]">
+            <Link as={`${link}`} href="[link]" target="_blank">
               <a className="hover:underline">{title}</a>
             </Link>
           </h3>
