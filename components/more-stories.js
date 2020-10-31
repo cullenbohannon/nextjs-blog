e30bb3be112caebb06b3dc32be3404a893f72f1e
cullenbohannon <65356707+cullenbohannon@users.noreply.github.com>
2020-10-31 12:42:08 +0000

    Update more-stories.js

--- components/more-stories.js
+++ components/more-stories.js
@@ -6,7 +6,7 @@ export default function MoreStories({ posts }) {
       <h2 className="text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
         Mai multe dume, nu rata!
       </h2>
-      <h3 className="mb-8 text-2xl md:text-3xl tracking-tighter leading-tight">Click pe imagine sau pe link sa lasi un raspuns!</h3>
+      <h3 className="mb-8 text-2xl md:text-3xl tracking-tighter leading-tight">Click pe imagine sau pe titlu sa lasi un raspuns!</h3>
       <div className="grid grid-cols-1 md:grid-cols-2 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-32 mb-32">
         {posts.map((post) => (
           <PostPreview
